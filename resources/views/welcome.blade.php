39a90be9f4d091bb24a3dbd5de57e11e11281b83
Niels Havermans <nielshavermans@ibizz.nl>
2022-12-01 15:29:39 +0100

    fix type

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,7 +24,8 @@
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
 
             <p><strong>Woonhome Edit API</strong></p>
-
+            <br/>
+            <br/>
             <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                 Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }}) - <a href="https://woonhome.nl/" title="Woonhome">woonhome.nl</a>
             </div>
