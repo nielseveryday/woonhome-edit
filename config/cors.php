23532c631724cb266441b851cb1ebdff64a014c2
Niels Havermans <nielshavermans@ibizz.nl>
2022-12-05 11:25:38 +0100

    change in cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*', '*,*', 'http://172.23.11.214:3000, *'],
 
     'allowed_origins_patterns' => [],
 
