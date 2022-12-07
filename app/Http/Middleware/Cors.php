21f790c6b15f8ba703c5643b5b65eac4754d8e4e
Niels Havermans <nielshavermans@ibizz.nl>
2022-12-07 13:27:29 +0100

    more cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -32,7 +32,10 @@ class Cors
         */
 
         //return $next($request);
+
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*');
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', "Accept,authorization,Authorization, Content-Type");
     }
 }
