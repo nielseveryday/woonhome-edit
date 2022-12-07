0d26a7dc0ee38783a040beda59fb03a02ebf94aa
Niels Havermans <nielshavermans@ibizz.nl>
2022-12-07 13:42:38 +0100

    remove middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -23,7 +23,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\Cors::class,
+        //\App\Http\Middleware\Cors::class,
     ];
 
     /**
@@ -66,6 +66,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'cors' => \App\Http\Middleware\Cors::class,
+        //'cors' => \App\Http\Middleware\Cors::class,
     ];
 }
